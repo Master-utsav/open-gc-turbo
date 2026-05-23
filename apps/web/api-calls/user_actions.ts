171c9e828226ffedf08b902aef22d1852a2d594e
master-utsav <utsavjaiswal890@gmail.com>
2026-05-23 21:03:37 +0530

    fixing_redis

--- apps/web/api-calls/user_actions.ts
+++ apps/web/api-calls/user_actions.ts
@@ -6,7 +6,6 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_HTTP_API_URL! || "https://localhost:3001/api/v1",
 });
 
-
 export async function createUser(formData: FormData) {
   const email = formData.get("email") as string;
   const name = formData.get("name") as string;
